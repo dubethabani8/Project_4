711ea5ac1fc67be8267802cf88199c2e6eb6b36a
Thabani Dube <tdube2@u.rochester.edu>
2019-04-09 23:32:21 -0400

    Lookup working. Insert, almost there
    Just focusing on CSG first then the rest will be easy pattern of the same thing.  So I changed around CSG stuff.
    Lookup working and insert and delete will be simple.

--- src/Project_4.c
+++ src/Project_4.c
@@ -13,5 +13,11 @@
 #include "header.h"
 
 int main(void) {
+	TUPLELIST_CSG csgT = newCSG("CSC173", "30849952", "A");
+	TUPLELIST_CSG csgN = newCSG("CSC173", "30849952", "B");
+	csgT->next = csgN;
+	TABLE_CSG[73] = csgT;
+	printf("LOOKUP RESULT: %d\n", lookup_CSG("CSC173", "*", "C"));
+
 	return EXIT_SUCCESS;
 }
--- src/part_1.c
+++ src/part_1.c
@@ -7,9 +7,9 @@
 //(Course-StudentId-Grade)
 typedef struct TUPLE_CSG *TUPLELIST_CSG;
 struct TUPLE_CSG {
-char Course[6];
-char StudentId[8];
-char Grade[2];
+char * Course;
+char * StudentId;
+char * Grade;
 TUPLELIST_CSG next;
 };
 typedef TUPLELIST_CSG HASHTABLE_CSG[197];
@@ -64,7 +64,7 @@ HASHTABLE_CR TABLE_CR;
 int hash(char * key); //function that returns the index of the key
 int string_int(char s[30]); //Function that returns sum of char values of string
 //insert functions
-void insert_CSG(char Course[6], char StudentId[8], char Grade[2]);
+void insert_CSG(char * Course, char * StudentId, char * Grade);
 void insert_SNAP(char StudentId[8], char Name[30], char Address[50], char Phone[8]);
 void insert_CP(char Course[6], char Prerequisite[6]);
 void insert_CDH(char Course[6], char Day[2], char Hour[4]);
@@ -78,42 +78,61 @@ void delete_CDH(char Course[6], char Day[2], char Hour[4]);
 void delete_CR(char Course[6], char Room[20]);
 
 //lookup functions
-int lookup_CSG(char Course[6], char StudentId[8], char Grade[2]);
+int lookup_CSG(char * Course, char * StudentId, char * Grade);
 int lookup_SNAP(char StudentId[8], char Name[30], char Address[50], char Phone[8]);
 int lookup_CP(char Course[6], char Prerequisite[6]);
 int lookup_CDH(char Course[6], char Day[2], char Hour[4]);
 int lookup_CR(char Course[6], char Room[20]);
 
-void insert_CSG(char Course[6], char StudentId[8], char Grade[2]){
+void toString_CSG(TUPLELIST_CSG csg);
+
+
+void insert_CSG(char * Course, char * StudentId, char * Grade){
 	//Make csg struct and insert if not already there
-	TUPLELIST_CSG csg;
-	if(strcmp(Course, "*") == 0)
-		strcpy(csg->Course,"");
-	else strcpy(csg->Course, Course);
-	if(strcmp(StudentId, "*") == 0)
-		strcpy(csg->StudentId,"");
-	else strcpy(csg->Grade, Grade);
-	if(strcmp(Grade, "*") == 0)
-		strcpy(csg->Grade,"");
-	else strcpy(csg->Grade, Grade);
-	//lookup csg. If not found then insert. Otherwise return
+	int present = lookup_CSG(Course, StudentId, Grade);
+	if(!present){
+		//insert into hashtable
+	}
+	else return;
 }
 
-int lookup_CSG(char Course[6], char StudentId[8], char Grade[2]){ //Key is Course-Student
-	//No stars in key components - get key and search that bucket for the tuple
+int lookup_CSG(char * Course, char * StudentId, char * Grade){ //Key is Course-Student
+	//If possible, get key and search that bucket for the tuple quickly
 	if(strcmp("*", Course) != 0 && strcmp("*", StudentId) != 0){
-	char * key = strcat(Course, StudentId);
+	char key[strlen(Course)];
+	strcpy(key, Course);
+	strcat(key, StudentId);
 	int index = hash(key);
 	TUPLELIST_CSG csg = TABLE_CSG[index];
-	//If csg == NULL, return 0, else
-		//while tuple is not found in bucket and csg->next is not NULL: search that linked list for csg with matching values
-		//return accordingly
+	if(csg == NULL) return 0;
+	else {
+		while(csg != NULL){
+			if(strcmp(csg->Grade, Grade) == 0) return 1;
+			else csg = csg->next;
+		}
+		return 0;
 	}
-	else {}  //If one of the components making up the key is *, then search all the buckets //Could use secondary indexing??
+	}
+	else { //Search all buckets if key cannot be used. Could use secondary indexing???
+		for(int i=0; i<197; i++){
+			TUPLELIST_CSG csg = TABLE_CSG[i];
+			if(csg == NULL) continue;
+			else{
+				while(csg != NULL){
+					if((strcmp(csg->Course, Course) == 0 || strcmp(Course, "*") == 0)&&
+					(strcmp(csg->StudentId, StudentId) == 0 || strcmp(StudentId, "*") == 0) &&
+					(strcmp(csg->Grade, Grade) == 0 || strcmp(Grade, "*") == 0))
+					return 1;
+				else csg = csg->next;
+				}
+				return 0;
+			}
+		}
+	}  
 
 }
 
-int hash(char * key){ //function that 
+int hash(char * key){ //function that returns the index of the key
 	int index = string_int(key);
 	return index % 197;
 }
@@ -126,3 +145,19 @@ int string_int(char * string){ //Function that returns sum of chars in string
 	return sum;
 }
 
+TUPLELIST_CSG newCSG(char * Course, char * StudentId, char * Grade){ //Create new CSG tuple
+	TUPLELIST_CSG csg;
+	csg = (struct TUPLE_CSG *) malloc(sizeof(struct TUPLE_CSG));
+	csg->Course = (char*) malloc(strlen(Course)* sizeof(char));
+	csg->StudentId = (char*) malloc(strlen(StudentId)* sizeof(char));
+	csg->Grade = (char*) malloc((strlen(Grade))* sizeof(char));
+	strcpy(csg->Course, Course);
+	strcpy(csg->StudentId, StudentId);
+	strcpy(csg->Grade, Grade);
+	csg->next = NULL;
+	return csg;
+}
+
+void toString_CSG(TUPLELIST_CSG csg){ //print CSG tuple
+	printf("|Course: %s | StudentId: %s | Grade: %s|\n", csg->Course, csg->StudentId, csg->Grade);
+}
